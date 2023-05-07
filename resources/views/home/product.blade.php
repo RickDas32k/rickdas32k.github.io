da8beefeaf62c6c37914ccb085318a480899250d
RickDas32k <117833906+RickDas32k@users.noreply.github.com>
2023-05-07 16:04:28 +0530

    Revert "Revert "laravel""
    This reverts commit 2d3fcaa8c682439c85bfa1802e67a551ca68cc1b.

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -6,48 +6,15 @@
                </h2>
             </div>
             <div class="row">
-               @foreach($product as $products)
 
-               <div class="col-sm-6 col-md-4 col-lg-4">
+         @foreach($product as $products)
+            <div class="col-sm-6 col-md-4 col-lg-4">
                   <div class="box">
                      <div class="option_container">
                         <div class="options">
-                           <a href="{{url('product_details',$products->id)}}" class="option1">
+                           <a href="" class="option1">
                            product details
                            </a>
-                             
-                           <form action="{{url('add_cart',$products->id)}}" method="post">
-
-                              @csrf
-
-                              <div class="row">
-
-                                 <div class="col-md-4">
-
-                                    <input type="number" name="Quantity" vale="1" min="1" style="width: 100px"> 
-
-                                 </div>
-                              
-                                 <div class="col-md-4">
-
-                                    <input type="submit" value="Add to Cart">
-                                 </div>
-
-                                 <input type="submit" value="Add to Cart">
-                              </div>
-
-                           </form>
-                        </div>
-                     </div>
-                     <div class="img-box">
-                        <img src="product/{{$products->image}}" alt="">
-                     </div>
-                     <div class="detail-box">
-                        <h5>
-                           {{$products->title}}
-                        </h5>
-
-                        @if($products->discount_price!=null)
                            <form action="" method="post">
                               
                               <div class="row">
@@ -61,24 +28,50 @@
                            </form>
                         </div>
                      </div>
-                        
-                        <h6 style="text-decoration: line-through; color: blue">
-                        Price<br>
+                     <div class="img-box">
+                        <img src="product/{{$products->image}}" alt="">
+                     </div>
+                     <div class="detail-box">
+                        <h5>
+                           {{$products->title}}
+                        </h5>
+
+                        @if($products->discount_price!=null)
+
+                        <h6 style="color: darkred;">
+                           Discount Price
+                           <br>
+                           ₹{{$products->discount_price}}
+                        </h6>
+
+                        <h6 style="text-decoration: line-through; color: blue;">
+                           Price
+                           <br>
                            ₹{{$products->price}}
                         </h6>
+
                         @else
-                        <h6 style="color: blue">
-                        Price<br>
+
+                        <h6 style="color: blue;">
+                           Price
+                           <br>
                            ₹{{$products->price}}
                         </h6>
+
                         @endif
+
+                        
                      </div>
                   </div>
                </div>
 
-
                @endforeach
+
                <span style="padding-top: 20px;">
+
                {!!$product->withQueryString()->links('pagination::bootstrap-5')!!}
+
+            </span>
+            
          </div>
-      </section>
+</section>
