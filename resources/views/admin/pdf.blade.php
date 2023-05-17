d3e18beb2ba222fb5ae2a80e88faea3e84ed2098
RickDas32k <117833906+RickDas32k@users.noreply.github.com>
2023-05-17 23:13:05 +0530

    RICK

--- resources/views/admin/pdf.blade.php
+++ resources/views/admin/pdf.blade.php
@@ -3,173 +3,118 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="home/css/pdf.css">
-    
-    <title>Order PDF</title>
+    <link rel="stylesheet" href="admin/assets/css/pdf.css">
+    <link rel="stylesheet" type="text/css" href="home/css/bootstrap.css" />
+      <!-- font awesome style -->
+      <!-- Custom styles for this template -->
+      
+      <!-- responsive style -->
+      
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<title>Order PDF</title>
 </head>
 <body>
 
-<div class="page-content container">
-    <div class="page-header text-blue-d2">
-        <h1 class="page-title text-secondary-d1">
-            Invoice
-            <small class="page-info">
-                <i class="fa fa-angle-double-right text-80"></i>
-                ID: #111-222
-            </small>
-        </h1>
 
-        <div class="page-tools">
-            <div class="action-buttons">
-                <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
-                    <i class="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
-                    Print
-                </a>
-                <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="PDF">
-                    <i class="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
-                    Export
-                </a>
-            </div>
-        </div>
-    </div>
-
-    <div class="container px-0">
-        <div class="row mt-4">
-            <div class="col-12 col-lg-12">
-                <div class="row">
-                    <div class="col-12">
-                        <div class="text-center text-150">
-                        <img width="250" src="images/logo.png" alt="#" />
+<div class="container">
+<div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="invoice-title">
+                        <h4 class="float-end font-size-15">Invoice # 1000{{$order->id}} <span class="badge bg-success font-size-12 ms-2">Paid</span></h4>
+                        <div class="mb-4">
+                        <a class="navbar-brand" href="{{url('/')}}"><img width="250" src="images/logo.png" alt="#" /></a>
                         </div>
-                    </div>
-                </div>
-                <!-- .row -->
-
-                <hr class="row brc-default-l1 mx-n1 mb-4" />
-
-                <div class="row">
-                    <div class="col-sm-6">
-                        <div>
-                            <span class="text-sm text-grey-m2 align-middle">To:</span>
-                            <span class="text-600 text-110 text-blue align-middle">Alex Doe</span>
-                        </div>
-                        <div class="text-grey-m2">
-                            <div class="my-1">
-                                Street, City
-                            </div>
-                            <div class="my-1">
-                                State, Country
-                            </div>
-                            <div class="my-1"><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> <b class="text-600">111-111-111</b></div>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-
-                    <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
-                        <hr class="d-sm-none" />
-                        <div class="text-grey-m2">
-                            <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125">
-                                Invoice
-                            </div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID:</span> #111-222</div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Issue Date:</span> Oct 12, 2019</div>
-
-                            <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Status:</span> <span class="badge badge-warning badge-pill px-25">Unpaid</span></div>
-                        </div>
-                    </div>
-                    <!-- /.col -->
-                </div>
-
-                <div class="mt-4">
-                    <div class="row text-600 text-white bgc-default-tp1 py-25">
-                        <div class="d-none d-sm-block col-1">#</div>
-                        <div class="col-9 col-sm-5">Description</div>
-                        <div class="d-none d-sm-block col-4 col-sm-2">Qty</div>
-                        <div class="d-none d-sm-block col-sm-2">Unit Price</div>
-                        <div class="col-2">Amount</div>
-                    </div>
-
-                    <div class="text-95 text-secondary-d3">
-                        <div class="row mb-2 mb-sm-0 py-25">
-                            <div class="d-none d-sm-block col-1">1</div>
-                            <div class="col-9 col-sm-5">Domain registration</div>
-                            <div class="d-none d-sm-block col-2">2</div>
-                            <div class="d-none d-sm-block col-2 text-95">$10</div>
-                            <div class="col-2 text-secondary-d2">$20</div>
-                        </div>
-
-                        <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-                            <div class="d-none d-sm-block col-1">2</div>
-                            <div class="col-9 col-sm-5">Web hosting</div>
-                            <div class="d-none d-sm-block col-2">1</div>
-                            <div class="d-none d-sm-block col-2 text-95">$15</div>
-                            <div class="col-2 text-secondary-d2">$15</div>
-                        </div>
-
-                        <div class="row mb-2 mb-sm-0 py-25">
-                            <div class="d-none d-sm-block col-1">3</div>
-                            <div class="col-9 col-sm-5">Software development</div>
-                            <div class="d-none d-sm-block col-2">--</div>
-                            <div class="d-none d-sm-block col-2 text-95">$1,000</div>
-                            <div class="col-2 text-secondary-d2">$1,000</div>
-                        </div>
-
-                        <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
-                            <div class="d-none d-sm-block col-1">4</div>
-                            <div class="col-9 col-sm-5">Consulting</div>
-                            <div class="d-none d-sm-block col-2">1 Year</div>
-                            <div class="d-none d-sm-block col-2 text-95">$500</div>
-                            <div class="col-2 text-secondary-d2">$500</div>
+                        <div class="text-muted">
+                            <p class="mb-1"><strong>ADDRESS:</strong> Chuchura, Hooghly, WB, India</p>
+                            <p class="mb-1"><i class="uil uil-envelope-alt me-1"></i> <strong>EMAIL:</strong> yourfamms@gmail.com</p>
+                            <p><i class="uil uil-phone me-1"></i> <strong>MOBILE:</strong> +91 62X XXX XXXX</p>
                         </div>
                     </div>
 
-                    <div class="row border-b-2 brc-default-l2"></div>
-                    <div class="row mt-3">
-                        <div class="col-12 col-sm-7 text-grey-d2 text-95 mt-2 mt-lg-0">
-                            Extra note such as company or payment information...
+                    <hr class="my-4">
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="text-muted">
+                                <h5 class="font-size-16 mb-3">Billed To:</h5>
+                                <h5 class="font-size-15 mb-2">{{$order->name}}</h5>
+                                <p class="mb-1">{{$order->address}}</p>
+                                <p class="mb-1">{{$order->email}}</p>
+                                <p>{{$order->phone}}</p>
+                            </div>
                         </div>
-
-                        <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
-                            <div class="row my-2">
-                                <div class="col-7 text-right">
-                                    SubTotal
+                        <!-- end col -->
+                        <div class="col-sm-6">
+                            <div class="text-muted text-sm-end">
+                                <div>
+                                    <h5 class="font-size-15 mb-1">Invoice No:</h5>
+                                    <p>#1000{{$order->id}}</p>
                                 </div>
-                                <div class="col-5">
-                                    <span class="text-120 text-secondary-d1">$2,250</span>
+                                <div class="mt-4">
+                                    <h5 class="font-size-15 mb-1">Invoice Date:</h5>
+                                    <p>{{ date('d / m / Y')}}</p>
                                 </div>
-                            </div>
-
-                            <div class="row my-2">
-                                <div class="col-7 text-right">
-                                    Tax (10%)
-                                </div>
-                                <div class="col-5">
-                                    <span class="text-110 text-secondary-d1">$225</span>
-                                </div>
-                            </div>
-
-                            <div class="row my-2 align-items-center bgc-primary-l3 p-2">
-                                <div class="col-7 text-right">
-                                    Total Amount
-                                </div>
-                                <div class="col-5">
-                                    <span class="text-150 text-success-d3 opacity-2">$2,475</span>
+                                <div class="mt-4">
+                                    <h5 class="font-size-15 mb-1">Order No:</h5>
+                                    <p>#1123456</p>
                                 </div>
                             </div>
                         </div>
+                        <!-- end col -->
                     </div>
+                    <!-- end row -->
+                    
+                    <div class="py-2">
+                        <h5 class="font-size-15">Order Summary</h5>
 
-                    <hr />
-
-                    <div>
-                        <span class="text-secondary-d1 text-105">Thank you for your business</span>
-                        <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Pay Now</a>
+                        <div class="table-responsive">
+                            <table class="table align-middle table-nowrap table-centered mb-0">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 70px;">No.</th>
+                                        <th>Item</th>
+                                        <th>Price</th>
+                                        <th>Quantity</th>
+                                        <th class="text-end" style="width: 120px;">Total</th>
+                                    </tr>
+                                </thead><!-- end thead -->
+                                <tbody>
+                                    <tr>
+                                        <th scope="row">01</th>
+                                        <td>
+                                            <div>
+                                                <h5 class="text-truncate font-size-14 mb-1">{{$order->product_title}}</h5>
+                                                
+                                            </div>
+                                        </td>
+                                        <td>MRP. {{$order->price}}/-</td>
+                                        <td>{{$order->quantity}}</td>
+                                        <td class="text-end">MRP. {{$order->price}}.00/-</td>
+                                    </tr>
+                                    <!-- end tr -->
+                                    <tr>
+                                        <th scope="row" colspan="4" class="text-end">Sub Total</th>
+                                        <td class="text-end">MRP. {{$order->price}}.00/-</td>
+                                    </tr>
+                                    <!-- end tr -->
+                            
+                                    <tr>
+                                        <th scope="row" colspan="4" class="border-0 text-end">Total</th>
+                                        <td class="border-0 text-end"><h4 class="m-0 fw-semibold">MRP. {{$order->price}}.00/-</h4></td>
+                                    </tr>
+                                    <!-- end tr -->
+                                </tbody><!-- end tbody -->
+                            </table><!-- end table -->
+                        </div><!-- end table responsive -->
+                        
                     </div>
                 </div>
             </div>
-        </div>
+        </div><!-- end col -->
     </div>
 </div>
 </body>
